618992561f941fa968a40a62074e4dd105c7090b
Kushal R <kushalr073@gmail.com>
2024-08-02 21:07:50 +0530

    first commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <section className="about container">
+            <h4>Events</h4>
+            <h2>ABOUT</h2>
+            <p>
+                Raj's Event Planner, established in 1997 and located near the BDA Complex in Banashankari, Bangalore, has been a leading name in the event planning industry for decades. Specializing in transforming events into unforgettable experiences, they offer a comprehensive range of services including venue selection, decor, catering, entertainment, and logistics. Known for their creativity and meticulous attention to detail, Raj's Event Planner ensures each event is tailored to the client's unique vision. Their strategic location provides access to the best vendors and venues in Bangalore, guaranteeing top-notch quality and service. With a reputation for reliability and excellence, Raj's Event Planner has earned the trust of countless clients over the years, making them the go-to choice for any event, be it a grand wedding or a corporate gathering.
+            </p>
+        </section>
+    );
+};
+
+export default About;
